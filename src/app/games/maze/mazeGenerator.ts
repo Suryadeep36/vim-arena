c4effad5b9e4857b69919d3bd62c4fd1b13ca4b8
Suryadeep36 <gohilsuryadeep3101@gmail.com>
2025-03-03 21:01:27 +0530

    add maze logic

---
+++ src/app/games/maze/mazeGenerator.ts
@@ -0,0 +1,64 @@
+export type Grid = {
+    top: boolean;
+    bottom: boolean;
+    left: boolean;
+    right: boolean;
+  };
+  
+  const options = [
+    [-1, 0],
+    [1, 0],
+    [0, 1],
+    [0, -1],
+  ];
+  
+  const SIZE = 25;
+  
+  export function generateMaze(): Grid[][] {
+    const gridArray: Grid[][] = Array.from({ length: SIZE }, () =>
+      Array.from({ length: SIZE }, () => ({
+        top: true,
+        bottom: true,
+        left: true,
+        right: true,
+      }))
+    );
+  
+    const isVisited: boolean[][] = Array.from({ length: SIZE }, () =>
+      Array(SIZE).fill(false)
+    );
+  
+    function connectBothGrid(i: number, j: number, newI: number, newJ: number) {
+      if (newI === i - 1) {
+        gridArray[i][j].top = false;
+        gridArray[newI][newJ].bottom = false;
+      } else if (newI === i + 1) {
+        gridArray[i][j].bottom = false;
+        gridArray[newI][newJ].top = false;
+      } else if (newJ === j - 1) {
+        gridArray[i][j].left = false;
+        gridArray[newI][newJ].right = false;
+      } else if (newJ === j + 1) {
+        gridArray[i][j].right = false;
+        gridArray[newI][newJ].left = false;
+      }
+    }
+  
+    function createMaze(i: number, j: number) {
+      isVisited[i][j] = true;
+      let directions = [...options].sort(() => Math.random() - 0.5); // Shuffle directions
+  
+      for (const [di, dj] of directions) {
+        let newI = i + di;
+        let newJ = j + dj;
+        if (newI >= 0 && newI < SIZE && newJ >= 0 && newJ < SIZE && !isVisited[newI][newJ]) {
+          connectBothGrid(i, j, newI, newJ);
+          createMaze(newI, newJ);
+        }
+      }
+    }
+  
+    createMaze(0, 0);
+    return gridArray;
+  }
+  
